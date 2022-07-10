50a82da18b0de80f86830e2a375a4bf23d2e5c64
Shikhar Saxena <danteapitome@gmail.com>
2022-07-10 22:53:39 +0530

    Creation of new Project and DTOs and Enum

---
+++ src/main/java/com/udemy/microservices/dto/BeerDTO.java
@@ -0,0 +1,29 @@
+package com.udemy.microservices.dto;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import com.udemy.microservices.util.BeerStyleEnum;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+
+	private UUID id;
+	private Integer version;
+	private OffsetDateTime createdDate;
+	private OffsetDateTime lastModifiedDate;
+	private String beerName;
+	private BeerStyleEnum beerStyle;
+	private Long upc;
+	private BigDecimal price;
+	private Integer quantityOnHand;
+}
---
+++ src/main/java/com/udemy/microservices/util/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package com.udemy.microservices.util;
+
+public enum BeerStyleEnum {
+
+	Lager, Pilsner, Stout, Gose, Porter, Ale, Wheat, IPA, Pale_Ale, Saison 
+}
